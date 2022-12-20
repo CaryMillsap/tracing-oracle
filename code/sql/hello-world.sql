ea32264520941ba04460466ec0d3be42bf6b7d94
Cary Millsap <cary.millsap@method-r.com>
2022-12-20 10:33:54 -0600

    8562

--- code/sql/hello-world.sql
+++ code/sql/hello-world.sql
@@ -1,10 +1,14 @@
 -- "hello, world", your first trace
 
--- If you can connect as system, then you won't need any additional grants to
--- run this script. Otherwise, if you connect as USERNAME, you'll need a DBA to
--- grant the following:
--- - grant execute on dbms_session to USERNAME
--- - grant alter session to USERNAME
+/*
+  If you can connect as system, then you won't need any additional grants to
+  run this script. Otherwise, if you connect as USERNAME, you'll need a DBA to
+  grant the following:
+
+    grant execute on dbms_session to USERNAME
+    grant alter session to USERNAME
+
+*/
 
 connect system
 
