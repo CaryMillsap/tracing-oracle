a08860d0c7f87df30abcc6b112f566ca91368667
Cary Millsap <cary.millsap@method-r.com>
2022-12-20 10:12:06 -0600

    8566

--- code/sql/inventory.sh
+++ code/sql/inventory.sh
@@ -2,8 +2,12 @@
 
 # Create script inventory.
 
+# Assumptions:
+# - All files have /.sql/ name suffix.
+# - First line of each file matches /^-- program description/.
+#
 for FILENAME in *.sql
 do
-	echo "$FILENAME" $(head -1 $FILENAME | sed "s/--/-/g")
+	echo "$FILENAME" $(head -1 $FILENAME | sed "s/^--/-/g")
 done | nl
 
