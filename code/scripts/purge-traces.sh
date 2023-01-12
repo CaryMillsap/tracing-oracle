0f337c069c808f79b4a07a3d691be3b9ad8f2887
Cary Millsap <cary.millsap@method-r.com>
2023-01-12 11:18:02 -0600

    lsof #TODO

--- code/scripts/purge-traces.sh
+++ code/scripts/purge-traces.sh
@@ -3,5 +3,7 @@
 TRCDIR=/DIR/diag/rdbms/DBNAME/INSTNAME/trace/	# Your .trc directory name goes here
 N=14											# 7, 14, 28 are good numbers for most people
 
+# #TODO - Use lsof to ensure that we're not going to delete an open file.
+
 find $TRCDIR -type f -name \*.tr[cm] -mtime +$N -delete
 
