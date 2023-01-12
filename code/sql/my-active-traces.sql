a986424021da0bc3aa5bce5164a5c0b681f527e8
Cary Millsap <cary.millsap@method-r.com>
2023-01-12 11:35:44 -0600

    Reformatted

--- code/sql/my-active-traces.sql
+++ code/sql/my-active-traces.sql
@@ -1,16 +1,16 @@
 -- print info about my active traces
 
 select
-	sid						sid,
-	serial#					serial,
-	service_name			service,
-	module					module,
-	action					action,
-	client_identifier		clientid,
-	sql_trace				trace,
-	sql_trace_waits			waits,
-	sql_trace_binds			binds,
-	sql_trace_plan_stats	planstats
+	  sid					sid
+	, serial#				serial
+	, service_name			service
+	, module				module
+	, action				action
+	, client_identifier		clientid
+	, sql_trace				trace
+	, sql_trace_waits		waits
+	, sql_trace_binds		binds
+	, sql_trace_plan_stats	planstats
 from
 	v$session
 where
