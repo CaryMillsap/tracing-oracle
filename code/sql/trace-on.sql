c7c8bfcb743dac11b7c4638cdea1d60fd5008955
Cary Millsap <cary.millsap@method-r.com>
2023-02-03 23:26:44 -0600

    Removed @my-trace-file

--- code/sql/trace-on.sql
+++ code/sql/trace-on.sql
@@ -2,7 +2,6 @@
 
 alter session set max_dump_file_size=unlimited;
 alter session set statistics_level=typical;
-select value file from v$diag_info where name='Default Trace File';
 exec dbms_session.session_trace_enable;
 
 
