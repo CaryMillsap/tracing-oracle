17968afe766d5e47e7567ba66a0fc9c8a9509514
Cary Millsap <cary.millsap@method-r.com>
2023-02-06 17:09:51 -0600

    .

--- code/experiments/session_cloud_trace/t.sql
+++ code/experiments/session_cloud_trace/t.sql
@@ -1,15 +1,19 @@
--- test session_cloud_trace
+-- demonstrate the permissions required for tracing (ADB)
 
 
 connect admin
+
 drop user u1 cascade;
-grant connect to u1 identified by "???";
+grant connect to u1 identified by "Y0ur-Password-G0es-Here";
 grant create session to u1;
+
+grant alter session to u1;
 grant read on session_cloud_trace to u1;
 
-connect u1
+-- Test with the following...
+connect u1/Y0ur-Password-G0es-Here
 alter session set sql_trace=true;
 select 'hello, world' from dual;
 alter session set sql_trace=false;
-@adb/my-trace-contents
+@adb/my-trace-content
 
