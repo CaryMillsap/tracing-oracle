86f06744b27377ebecabe10f7a43bc03907a40fb
Cary Millsap <cary.millsap@method-r.com>
2023-02-06 17:09:34 -0600

    eliminated need for v$session, v$process

--- code/sql/my-trace-content.sql
+++ code/sql/my-trace-content.sql
@@ -2,16 +2,10 @@
 
 -- Copyright (c) 2019, 2022 Method R Corporation
 
-select trim(trailing chr(10) from payload) "TRACE LINES"
+select trim(trailing chr(10) from payload) "TRACE"
 from v$diag_trace_file_contents
 where adr_home || '/trace/' || trace_filename = (
-	select tracefile
-	from v$process
-	where addr = (
-		select paddr
-		from v$session
-		where sid = sys_context('userenv', 'sid')
-	)
+	select value from v$diag_info where name = 'Default Trace File'
 )
 order by line_number 
 /
