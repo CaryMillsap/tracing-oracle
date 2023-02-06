72e5738f58912905d2589c4df63b3c4ce4e4b8b1
Cary Millsap <cary.millsap@method-r.com>
2023-02-06 17:23:22 -0600

    ADB trace permissions for devs

---
+++ code/sql/adb/dev-trace-permissions.sql
@@ -0,0 +1,24 @@
+-- demonstrate the permissions required for tracing (Autonomous Database)
+
+-- You must be ADMIN to perform the required trants.
+-- connect admin
+
+drop user dev1 cascade;						-- Fresh start
+grant connect to dev1 identified by "Y0ur-Password-G0es-Here";
+grant create session to dev1;
+
+grant read on session_cloud_trace to dev1;	-- Required to read trace file content
+grant alter session to dev1;				-- Required to enable and disable tracing
+
+-- Connect for testing...
+connect dev1/Y0ur-Password-G0es-Here
+
+-- Ensure that the required views are accessible
+desc session_cloud_trace
+
+-- Full cycle: trace an application and access its content.
+@adb/trace-on
+select 'hello, from ADB' from dual;
+@adb/trace-off
+@adb/my-trace-content
+
