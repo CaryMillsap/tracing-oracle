89003595e390f5eef679460a01775defca8b9901
Cary Millsap <cary.millsap@method-r.com>
2023-02-07 17:04:58 -0600

    hello world refinements

--- code/sql/adb/hello-world.sql
+++ code/sql/adb/hello-world.sql
@@ -1,19 +1,14 @@
 -- "hello, world", your first trace (ADB)
 
-/*
+-- This script will ONLY run on an Oracle Autonomous Database system.
 
-This script will ONLY run on an Oracle Autonomous Database system.
-
-See adb/dev-trace-permissions.sql for information about required privileges
-if you can't connect as ADMIN.
-
-*/
+-- See adb/dev-trace-permissions.sql for information about required privileges
+-- if you can't connect as ADMIN.
 
 -- connect admin
 
 @adb/trace-on
 select 'hello, world' from dual;
 @adb/trace-off
-
 @adb/my-trace-content
 
--- code/sql/hello-world.sql
+++ code/sql/hello-world.sql
@@ -1,20 +1,16 @@
 -- "hello, world", your first trace
 
-/*
+-- This script will NOT run on an Oracle Autonomous Database system.
 
-This script will NOT run on an Oracle Autonomous Database system.
-
-See (dba|dev)-trace-permissions.sql for information about required privileges
-if you can't connect as SYSTEM.
-
-*/
+-- See (dba|dev)-trace-permissions.sql for information about required
+-- privileges if the following scripts fail for you.
 
 -- connect system
 
 @trace-on
 select 'hello, world' from dual;
 @trace-off
-
+@my-trace-dir
 @my-trace-file
 @my-trace-content
 
