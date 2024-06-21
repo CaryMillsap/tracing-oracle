82e0fe86a7b36c81fc483b8465a8caae74e5c8ca
Cary Millsap <cary.millsap@method-r.com>
2024-06-21 11:03:57 -0500

    cleanup and reporting

--- code/scripts/fill-disk.sh
+++ code/scripts/fill-disk.sh
@@ -2,19 +2,33 @@
 
 # Fill a disk (for testing disk-consumption-rate.pl)
 
-output_dir="dcrtest"
-output_file="fill_disk.img"
+output_dir="DELETE-ME"
+output_file="delete-me.img"
 output_path="$output_dir/$output_file"
-tmp_file="delete-me.img"
+tmp_file="tmp.img"
 
+cleanup() {
+   # One could clean up here, but that throws off the disk-consumption-rate.pl totals.
+   # Cleanup is thus left for the user to take care of.
+   #
+   # rm "$output_path"    # one component at a time, ...
+   # rmdir "$output_dir"  # ...just to be safe
+   printf "\n"
+   printf "$output_path\n"
+   ls -lh "./$output_dir" | tail -1
+   exit 0
+}
+trap cleanup SIGINT
+
+rm "$output_path"       # No need having the file start off big.
 mkdir -p "$output_dir"
 
 while true; do
-   dd if=/dev/urandom of="$tmp_file" bs=1024 count=100 conv=notrunc status=progress
+   dd if=/dev/urandom of="$tmp_file" bs=1024 count=100 conv=notrunc oflag=fsync status=none    # speed=2m
    cat "$tmp_file" >> "$output_path"
    rm "$tmp_file"
+   printf "."
    sleep 1
 done
 
-rm -rf "$output_dir"
 
