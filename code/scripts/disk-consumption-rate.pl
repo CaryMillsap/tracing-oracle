ecf77aec9c072b3c42a7b48ffe2d520dc7065437
Cary Millsap <cary.millsap@method-r.com>
2024-06-21 17:51:28 -0500

    New unit option

--- code/scripts/disk-consumption-rate.pl
+++ code/scripts/disk-consumption-rate.pl
@@ -13,25 +13,58 @@ use Pod::Usage;
 our $Program = fileparse $0, qr(\..*);
 
 our %Opt = (
-   cols     => 'kmgt',        # all columns: KB/s, MB/m, GB/h, TB/d
+   debug    => 0,
    ofile    => '',
+   unit     => 'MB/h',
 );
 our %Options = (
+   "debug"     => \$Opt{debug},
    "help|?"    => sub { pod2usage(-input=>\*main::DATA, -verbose => 1, -exit => 0); },
    "man"       => sub { pod2usage(-input=>\*main::DATA, -verbose => 2, -exit => 0); -formatter => "Pod::Text::Termcap"; },
    "usage"     => sub { pod2usage(-input=>\*main::DATA, -verbose => 0, -exit => 0); },
    #  
-   "cols=s" => \$Opt{cols},
-   "o"      => \$Opt{ofile},  # Not implemented (easy enough to do on the command line with `tee`).
+   "o"         => \$Opt{ofile},  # Not implemented (easy enough to do on the command line with `tee`).
+   "unit=s"    => \$Opt{unit},
 );
 GetOptions(%Options) or die;
 
+my $DEBUG = $Opt{debug};
+my $WORKING = 0;
+
+my %bytes = (
+   "B"   => 1,
+   "KB"  => 1e3,
+   "MB"  => 1e6,
+   "GB"  => 1e9,
+   "TB"  => 1e12,
+   "PB"  => 1e15,
+);
+my %seconds = (
+   "s"   => 1,
+   "m"   => 60,
+   "h"   => 60*60,
+   "d"   => 60*60*24,
+   "w"   => 60*60*24*7,
+);
+
+
+my ($qunit, $tunit);
+my ($bytes, $seconds);
+{
+   ($qunit, $tunit) = split('/', $Opt{unit});
+   $bytes   = $bytes{$qunit};
+   $seconds = $seconds{$tunit};
+   printf "Opt{unit}='%s'  qunit='%s' tunit='%s'  bytes='%d' seconds='%d'\n", $Opt{unit}, $qunit, $tunit, $bytes, $seconds if $DEBUG;
+   die "unrecognized quantity unit '$qunit'; should be one of (B KB MB GB TB PB)" if !defined $bytes;
+   die "unrecognized time unit '$tunit'; should be one of (s m h d w)"            if !defined $seconds;
+}
+
 my ($dir, $interval, $count) = (
    $ARGV[0] //  ".",          # Directory name.
-   $ARGV[1] //   10,          # Interval duration in seconds.
+   $ARGV[1] //   1,           # Interval duration in seconds.
    $ARGV[2] // 1000,          # Number of intervals.
 );
-printf "%s '%s' %d %d\n", $0, File::Spec->rel2abs($dir), $interval, $count;
+printf "%s '%s' %d %d\n\n", $0, File::Spec->rel2abs($dir), $interval, $count;
 
 my $format = "%Y-%m-%dT%H:%M:%S";      # ISO 8601.
 
@@ -40,47 +73,60 @@ sub d {
    chomp(my $d = qx(date "+$format")); # `date` prints a newline, so chomp it.
    return $d;
 }
-sub k {
-   # Execute the du command.
+sub du {
+   # Execute the du command, return bytes.
+   # ASSERT: `du -s` returns bytes used in the first field of its output.
+   # ASSERT: `du -k` returns 1024-byte blocks.
    return 0 if not -d "$dir";
-   chomp(my $k = qx(du -ks "$dir"));   # `du` prints a newline, so chomp it.
-   $k =~ s/^(\d+).*/$1/;               # `du` returns /\d+  $dir/.
-   return $k;
+   my $du = qx(du -ks "$dir");
+   return (split /\s+/, $du)[0] * 1024;
+}
+sub df { 
+   # Execute the df command, return bytes.
+   # ASSERT: `df` returns "Available" in the fourth field of the final line of its output.
+   # ASSERT: `df -k` returns 1024-byte blocks.
+   my $df = qx(df -k "$dir" | tail -1);
+   return (split /\s+/, $df)[3] * 1024;
 }
 sub p($$$$) {
-   # Print the rates from (d0,k0) to (d1,k1).
-   my ($d0, $k0, $d1, $k1) = @_;
-   my $s = Time::Piece->strptime($d1, $format) - Time::Piece->strptime($d0, $format);
+   # Print the rates from (date1,du1) to (date2,du2).
+   my ($date1, $du1, $date2, $du2) = @_;
+   my $s = Time::Piece->strptime($date2, $format) - Time::Piece->strptime($date1, $format);
    return if $s == 0;
-   my $k = $k1 - $k0;
-   my $kps = $k/$s;
-   printf "%s … %s = %4d s   ", $d0, $d1, $s;
-   printf "%9d KB … %9d KB = %9d KB", $k0, $k1, $k;
-   printf "  %10.1f KB/s", $kps              if $Opt{cols} =~ 'k';
-   printf "  %10.1f MB/m", $kps/1e3*60       if $Opt{cols} =~ 'm';
-   printf "  %10.1f GB/h", $kps/1e6*60*60    if $Opt{cols} =~ 'g';
-   printf "  %10.1f TB/d", $kps/1e9*60*60*24 if $Opt{cols} =~ 't';
-   printf "  %10d GB free", 0;
-   printf "  %10d days until full", 0;
+   my $du = $du2 - $du1;
+   my $bps = $du/$s;
+   my $df = df;
+   # Next stats are in s, irrespective of the chosen time unit.
+   printf "%s … %s = %4d s   ", $date1, $date2, $s;
+   # Next stats are in KB, irrespective of the chosen quantity unit.
+   printf "(%9d KB … %9d KB) = %9d KB", $du1/$bytes{KB}, $du2/$bytes{KB}, $du/$bytes{KB};
+   printf "  %10.1f %s/%s", $bps/($bytes/$seconds), $qunit, $tunit;
+   printf "  %10d %s free", $df/$bytes, $qunit;
+   printf "   ";
+   if ($bps <= 0) {
+      printf "not filling";
+   } else {
+      printf "%.1f days until full", $df/$bps/$seconds{d};
+   }
    print "\n";
 }
 $SIG{INT} = sub { exit; }; # Execute the END block upon Ctrl-C.
 
-my ($d00, $k00);           # First ever (date, kb) tuple.
-my ($d0, $k0);             # Prior (date, kb) tuple.
-my ($d1, $k1);             # Current (date, kb) tuple.
+my ($date0, $du0);         # First ever (date, du) tuple.
+my ($date1, $du1);         # Prior (date, du) tuple.
+my ($date2, $du2);         # Current (date, du) tuple.
 
-($d00, $k00) = ($d0, $k0) = ($d1, $k1) = (d, k);
+($date0, $du0) = ($date1, $du1) = ($date2, $du2) = (d, du);
 for (1..$count) {
    sleep($interval);
-   ($d1, $k1) = (d, k);
-   p($d0, $k0, $d1, $k1);
-   ($d0, $k0) = ($d1, $k1);
+   ($date2, $du2) = (d, du);
+   p($date1, $du1, $date2, $du2);
+   ($date1, $du1) = ($date2, $du2);
 }
 
 END {
    print "\n";
-   p($d00, $k00, $d1, $k1);
+   p($date0, $du0, $date2, $du2);
 }
 
 
@@ -95,8 +141,6 @@ disk-consumption-rate - calculate disk consumption rate
 =head1 SYNOPSIS
 
 disk-consumption-rate [ I<options> ] [ I<directory> [ I<interval> [ I<count> ] ] ]
-
-  --cols=s     print X/second columns, where X in (k, m, g, t); default --cols=kmgt
 
 
 =head1 DESCRIPTION
@@ -118,6 +162,24 @@ summarizes all the disk consumption that has occurred since the program was
 executed. To terminate the program before it reaches the specified interval
 count, simply press Ctrl-C. The program will then print its summary line and
 terminate.
+
+
+=head1 OPTIONS
+
+=over
+
+=item --man
+
+Print the manual page and exit.
+
+=item --unit=I<string>
+
+Report in the given unit, expressed in "Q/T" format (quantity per unit of
+time). Quantity can be any of B, KB, MB, GB, TB, or PB. Time can be any of s,
+m, h, d, w (seconds, minutes, hours, days, weeks). Default is "GB/h".
+
+=back
+
 
 
 =head1 EXAMPLES
@@ -142,5 +204,5 @@ Cary Millsap
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (c) 2022 Method R Corporation
+Copyright (c) 2022, 2024 Cary Millsap
 
